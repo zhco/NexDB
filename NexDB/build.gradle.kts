f956093237e45c4dab10208f13bd305d5d5c4271
zhco <luycheung@qq.com>
2026-07-13 17:48:05 +0800

    AGP 8.7.3 + KSP

--- NexDB/build.gradle.kts
+++ NexDB/build.gradle.kts
@@ -1,5 +1,1 @@
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-}
+plugins{id("com.android.application")version"8.7.3"apply false;id("org.jetbrains.kotlin.android")version"2.1.0"apply false;id("org.jetbrains.kotlin.plugin.compose")version"2.1.0"apply false;id("com.google.devtools.ksp")version"2.1.0-1.0.29"apply false}
